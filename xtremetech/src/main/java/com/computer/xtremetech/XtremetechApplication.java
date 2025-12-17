4a3172e6c384bc56107a8f1a6c36184eb3e3f21d
“Thushara1020” <thusharabawantha2001@gmail.com>
2025-12-17 23:58:37 +0530

    Initial commit

---
+++ xtremetech/src/main/java/com/computer/xtremetech/XtremetechApplication.java
@@ -0,0 +1,13 @@
+package com.computer.xtremetech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XtremetechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XtremetechApplication.class, args);
+	}
+
+}
