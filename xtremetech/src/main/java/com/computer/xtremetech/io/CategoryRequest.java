43f33e4f8e62866646286de271254d3e4407a4e6
“Thushara1020” <thusharabawantha2001@gmail.com>
2025-12-18 02:04:44 +0530

    add CategoryRequest DTO for category creation

---
+++ xtremetech/src/main/java/com/computer/xtremetech/io/CategoryRequest.java
@@ -0,0 +1,15 @@
+package com.computer.xtremetech.io;
+
+
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+public class CategoryRequest {
+
+    private String name;
+    private String description;
+    private String bgColor;
+
+}
