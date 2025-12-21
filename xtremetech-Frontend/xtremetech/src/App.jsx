87f2d48f9ad415b1a01e97e26ed0d8c4035f1b23
“Thushara1020” <thusharabawantha2001@gmail.com>
2025-12-22 02:10:45 +0530

    initialize React App

---
+++ xtremetech-Frontend/xtremetech/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to Xtremetech Online Shop</h1>
+    </div>
+  );
+}
+
+export default App;
