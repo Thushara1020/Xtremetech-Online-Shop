4746c6c0106f36deafbe8ca5c63a4ef13305db51
“Thushara1020” <thusharabawantha2001@gmail.com>
2025-12-22 05:49:48 +0530

    Create responsive Menubar component using Bootstrap

--- xtremetech-Frontend/xtremetech/src/components/Menubar/Menubar.jsx
+++ xtremetech-Frontend/xtremetech/src/components/Menubar/Menubar.jsx
@@ -1,7 +1,52 @@
 const Menubar = () => {
     return (
-        <div>Menubar</div>
-    )
-}
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-2">
+            <a className="navbar-brand" href="#">
+                <img 
+                    src="https://www.svgrepo.com/show/303109/adobe-xd-logo.svg" 
+                    alt="Logo" 
+                    height="40" 
+                />
+            </a>
+            <button 
+                className="navbar-toggler" 
+                type="button" 
+                data-bs-toggle="collapse" 
+                data-bs-target="#navbarNav" 
+                aria-controls="navbarNav" 
+                aria-expanded="false" 
+                aria-label="Toggle navigation"
+            >
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse p-2" id="navbarNav">
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li className="nav-item">
+                        <a className="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">About</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Services</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Contact</a>
+                    </li>
+                </ul>
+                <form className="d-flex">
+
+                    <input 
+                        className="form-control me-2" 
+                        type="search" 
+                        placeholder="Search" 
+                        aria-label="Search" 
+                    />
+                    <button className="btn btn-outline-light" type="submit">Search</button>
+                </form>
+            </div>
+        </nav>
+    );
+};
 
 export default Menubar;
