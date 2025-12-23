462b15d44c99a9a55d0bcf2dd70f5330cc31685f
“Thushara1020” <thusharabawantha2001@gmail.com>
2025-12-23 23:02:02 +0530

    add routing logic for all main pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,26 @@
 import Menubar from "./components/Menubar/Menubar.jsx";
+import {Route, Routes} from "react-router-dom";
+import Dashboard from "./assets/pages/Dashboard/Dashboard.jsx";
+import ManageCategory from "./assets/pages/ManageCategory/ManageCategory.jsx";
+import ManageUsers from "./assets/pages/ManageUsers/ManageUsers.jsx";
+import ManageItems from "./assets/pages/ManageItems/ManageItems.jsx";
+import Explore from "./assets/pages/Explore/Explore.jsx";
 
 const App = () => {
     return (
         <div>
             <Menubar />
+            <Routes>
+                <Route path="/dashboard" element={<Dashboard/>} />
+                <Route path="/category" element={<ManageCategory/>} />
+                <Route path="/users" element={<ManageUsers/>} />
+                <Route path="/items" element={<ManageItems/>} />
+                <Route path="/explore" element={<Explore/>} />
+
+                <Route path="/" element={<Dashboard/>} />
+            </Routes>
         </div>
     )
 }
 
-export default App
+export default App;
