8f22e535a8228decbdd0e892fe96216f1c791093
“Thushara1020” <thusharabawantha2001@gmail.com>
2025-12-23 23:00:37 +0530

    switch to react-router-dom and update assets

--- client/src/components/Menubar/Menubar.jsx
+++ client/src/components/Menubar/Menubar.jsx
@@ -1,11 +1,13 @@
 import './Menubar.css';
+import {assets} from "../../assets/assets.js";
+import {Link} from "react-router-dom";
 
 
 const Menubar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-2">
             <a className="navbar-brand" href="#">
-                <img src="https://www.svgrepo.com/show/303109/adobe-xd-logo.svg" alt="Logo" height="40" />
+                <img src={assets.logo} alt="Logo" height="60" />
             </a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
@@ -13,19 +15,19 @@ const Menubar = () => {
             <div className="collapse navbar-collapse p-2" id="navbarNav">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                     <li className="nav-item">
-                        <a className="nav-link active" aria-current="page" href="#">Dashboard</a>
+                        <Link className="nav-link" to="/dashboard">Dashboard</Link>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#">Explore</a>
+                        <Link className="nav-link" to="/explore">Explore</Link>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#">Manage Items</a>
+                        <Link className="nav-link" to="/items">Manage Items</Link>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#">Manage Categorise</a>
+                        <Link className="nav-link" to="/category">Manage Categorise</Link>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#">Manage Users</a>
+                        <Link className="nav-link" to="/users">Manage Users</Link>
                     </li>
                 </ul>
                 {/*Add the Dropdown for User Profile*/}
