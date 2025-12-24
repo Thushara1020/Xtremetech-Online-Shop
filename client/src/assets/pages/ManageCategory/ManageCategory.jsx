5d6dd6baeaf476efed6e289b00ba132c9d441204
“Thushara1020” <thusharabawantha2001@gmail.com>
2025-12-24 21:39:20 +0530

    Integrated CategoryForm and CategoryList components

--- client/src/assets/pages/ManageCategory/ManageCategory.jsx
+++ client/src/assets/pages/ManageCategory/ManageCategory.jsx
@@ -1,6 +1,18 @@
+import './ManageCategory.css'
+import CategoryForm from "../../../components/CategoryForm/CategoryForm.jsx";
+import CategoryList from "../../../components/CategoryList/CategoryList.jsx";
+
 const ManageCategory = () => {
     return (
-        <div>ManageCategory</div>
+        <div className="category-container text-light">
+            <div className="left-column">
+                <CategoryForm/>
+            </div>
+
+            <div className="right-column">
+                <CategoryList/>
+            </div>
+        </div>
     )
 }
 
