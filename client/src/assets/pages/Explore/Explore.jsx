ba5717c75eb10b0641c838a4a4bbcd58c6c58866
“Thushara1020” <thusharabawantha2001@gmail.com>
2025-12-26 23:55:50 +0530

    implement layout structure for Explore page

--- client/src/assets/pages/Explore/Explore.jsx
+++ client/src/assets/pages/Explore/Explore.jsx
@@ -1,7 +1,29 @@
+import'./Explore.css';
+
 const Explore = () => {
     return (
-        <div>
-            Explore Page
+        <div className="explore-container text-light">
+            <div className="left-column">
+                <div className="fist-row" style={{overflowY: 'auto'}}>
+                    categories
+                </div>
+                <hr className="horizontal-line" />
+                <div className="second-row" style={{overflowY: 'auto'}}>
+                    items
+                </div>
+            </div>
+            <div className="right-column d-flex flex-column">
+                <div className="customer-form-container" style={{height:'15%'}}>
+                    costumer form
+                </div>
+                <hr className="my-3 text-light"/>
+                <div className="cart-items-contaner" style={{height:'55%', overflowY: 'auto'}}>
+                    cart items
+                </div>
+                <div className="cart-items-contaner" style={{height:'30%'}}>
+                    customer summary
+                </div>
+            </div>
         </div>
     )
 }
